76eef4ede6df800004f372373af4cfa7f740d4d6
Dan Ho <vidanho@gmail.com>
2017-10-25 12:19:31 -0500

    Made change to 'about' URL to 'about_us'

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get '/pages' => 'pages#home', as: 'home'
 
-  get '/pages/about' => 'pages#about', as: 'about'
+  get '/pages/about_us' => 'pages#about', as: 'about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
