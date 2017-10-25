0891913f6bdc18a8871ebcbea5337aa477f99a97
Dan Ho <vidanho@gmail.com>
2017-10-25 13:32:22 -0500

    Validation for color model. Seeded 100 colour objects to db

--- app/models/colour.rb
+++ app/models/colour.rb
@@ -1,7 +1,7 @@
 class Colour < ApplicationRecord
   validates :name, :hex, presence: true
   validates :name, :hex, uniqueness: true
-  validates :name, format: { with: /^[a-z]+$/ }
-  validates :hex, format: { with: /^#+[0-9]+$/ }
+  validates :name, format: { with: /\A[a-z]+\z/ }
+  validates :hex, format: { with: /\A#+[0-9]+\z/ }
   validates :hex, length: { is: 7 }
 end
