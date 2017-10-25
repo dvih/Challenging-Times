79dd977c95bcf6a25d85d18ac1ae2e1c4771ec15
Dan Ho <vidanho@gmail.com>
2017-10-25 12:22:58 -0500

    New lizard action. Fixed routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get '/pages' => 'pages#home', as: 'home'
+  get '' => 'pages#home', as: 'home'
 
-  get '/pages/about_us' => 'pages#about', as: 'about'
+  get 'about_us' => 'pages#about', as: 'about'
+
+  get 'lizard/:number', to: 'pages#lizards', as: 'lizards'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
